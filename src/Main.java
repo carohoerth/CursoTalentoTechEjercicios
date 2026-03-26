58075639b293848976b9b684ef3b1d78c75a92a2
Carolina <carolinahoerth@gmail.com>
2026-03-26 18:56:38 -0300

    agregando codigo inicial

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo desde CursoTalentoTech!");
+        System.out.println("Java 21 detectado y funcionando.");
+    }
+}
