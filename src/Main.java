4ec1cb05129829e8b0b2454df572b54f5ba5ed10
Carolina <carolinahoerth@gmail.com>
2026-04-09 19:12:00 -0300

    Agregando clase Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("¡Hola Mundo desde CursoTalentoTech!");
         System.out.println("Java 21 detectado y funcionando.");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = sc.nextLine();
+        System.out.println("Bienvenida " + nome);
+
     }
 }
